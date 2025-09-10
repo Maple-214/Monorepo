dac24febe97cfd8f5878a66f2bbfa0ff2ea73f41
Maple <200214zef@gmail.com>
2025-09-11 02:44:00 +0800

    chore: 移除test文件

--- apps/web/test/mocks/MockApp1.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function MockApp1() {
-  return <div data-testid="mock-app1">[Mock App1]</div>;
-}
--- apps/web/test/mocks/MockApp2.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function MockApp2() {
-  return <div data-testid="mock-app2">[Mock App2]</div>;
-}
